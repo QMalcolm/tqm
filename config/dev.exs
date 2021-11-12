028115143702700a48990f466b5950cf363b3b28
Quigley malcolm <tqmalcolm@protonmail.com>
2021-11-12 11:38:11 -0800

    Add tailwind css

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,15 @@ config :tqm, TqmWeb.Endpoint,
   secret_key_base: "+27FCHd98KQsKYY7AwHPRkq5AutSfwj2mxplYMeMdOkuf1qGxyI1M/fSvU1LvEgx",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support

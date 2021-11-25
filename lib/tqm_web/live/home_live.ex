1a2de082ddbf237c48c536a6fc07f5fd7a7e3982
Quigley malcolm <tqmalcolm@protonmail.com>
2021-11-25 12:40:43 -0800

    Add home page side bar

--- lib/tqm_web/live/home_live.ex
+++ lib/tqm_web/live/home_live.ex
@@ -1,10 +1,7 @@
 defmodule TqmWeb.HomeLive do
   use TqmWeb, :live_view
 
-  def render(assigns) do
-    ~L"""
-    <%= live_component @socket, TqmWeb.NavbarComponent %>
-    <h1>This is my website<h1>
-    """
+  def mount(_params, _session, socket) do
+    {:ok, socket}
   end
 end

e381064e0b912f8ec6fee940e44848db14c97a42
Quigley malcolm <tqmalcolm@protonmail.com>
2021-11-26 08:51:03 -0800

    Goeun left me a cute comment when I wasn't looking

--- lib/tqm_web/live/components/navbar_component.ex
+++ lib/tqm_web/live/components/navbar_component.ex
@@ -1,6 +1,8 @@
 defmodule TqmWeb.NavbarComponent do
   use TqmWeb, :live_component
 
+  # bbg loves bbq 11/26/2021 <3 <3 <3
+
   defp is_active_path(socket, nav_name) do
     view = socket.view
     case nav_name do

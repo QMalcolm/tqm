5c7405f103f9d1506f2a32b4cf16a726faf88d88
Quigley Malcolm <tqmalcolm@protonmail.com>
2023-02-04 16:01:18 -0800

    chore: fix home page controller test

--- test/tqm_web/controllers/page_controller_test.exs
+++ test/tqm_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule TqmWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Hi, welcome to my website!"
   end
 end

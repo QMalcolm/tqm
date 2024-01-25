90dd0d2bb8f22bf6f0b7238116d2416dc5445c8f
Quigley Malcolm <tqmalcolm@protonmail.com>
2024-01-24 19:25:34 -0800

    Upgrade `phoenix_html` dependency version
    I hoped this dependnecy would be the dependency needed to resove the
    issue mentioned in the previous commit. Alas, it was not. I did do a
    test of updating A BUNCH of the dependencies, and one of them did
    resovle it, I just don't know which... It makes sense that this
    dependency didn't do the trick, as it looks like one of my previous
    dependency updates already pulled in the latest version of this dep in
    the lock file.

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule Tqm.MixProject do
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 3.0"},
+      {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.18.3"},
       {:heroicons, "~> 0.5"},

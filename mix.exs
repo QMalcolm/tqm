db6ecd0f86955facac2960a2c16ea4066fa946c5
Quigley Malcolm <tqmalcolm@protonmail.com>
2024-01-24 19:07:58 -0800

    Upgrade Phoenix version to 1.7.10
    Interestingly I'm getting a bunch of warnings at compile time of the
    form
    ```
    warning: no route path for TqmWeb.Router matches ~p"/people/confirm/#{&1}"
      lib/tqm_web/live/person_confirmation_instructions_live.ex:33: TqmWeb.PersonConfirmationInstructionsLive.handle_event/3
    ```
    for a bunch of different route calls. Not sure at the moment what the
    problem is. Going to try and solve the issue in the next commit.

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Tqm.MixProject do
   defp deps do
     [
       {:bcrypt_elixir, "~> 3.0"},
-      {:phoenix, "~> 1.7.0-rc.2", override: true},
+      {:phoenix, "~> 1.7.10"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},

fda92b022fb5ef3025aadc353bc6a7adf208ba87
Quigley Malcolm <tqmalcolm@protonmail.com>
2026-05-03 16:36:17 -0500

    Deduplicate rss_feed and tag_rss_feed into build_rss/3
    Both functions rendered identical item XML and differed only in channel
    metadata. Extracting build_rss/3 removes the duplication and makes
    adding a new feed variant a one-line channel map rather than a full
    copy of the template.

--- lib/tqm_web/controllers/feed_controller.ex
+++ lib/tqm_web/controllers/feed_controller.ex
@@ -4,28 +4,46 @@ defmodule TqmWeb.FeedController do
   alias Tqm.Blog
 
   def index(conn, _params) do
+    base_url = TqmWeb.Endpoint.url()
+
     blog_posts =
       Blog.list_blog_posts(:published)
       |> Enum.sort_by(& &1.published_at, {:desc, DateTime})
 
+    channel = %{
+      title: "Quigley Malcolm",
+      description: "Blog posts by Quigley Malcolm",
+      self_link: "#{base_url}/blog/feed.xml"
+    }
+
     conn
     |> put_resp_content_type("application/rss+xml")
-    |> send_resp(200, rss_feed(blog_posts))
+    |> send_resp(200, build_rss(channel, blog_posts, base_url))
   end
 
   def tag(conn, %{"tag" => slug}) do
+    base_url = TqmWeb.Endpoint.url()
+
     blog_posts =
       Blog.list_blog_posts(:published, tag: slug)
       |> Enum.sort_by(& &1.published_at, {:desc, DateTime})
 
+    channel = %{
+      title: "Quigley Malcolm - #{xml_escape(slug)} posts",
+      description: "Blog posts tagged &#34;#{xml_escape(slug)}&#34; by Quigley Malcolm",
+      self_link: "#{base_url}/blog/tags/#{slug}/feed.xml"
+    }
+
     conn
     |> put_resp_content_type("application/rss+xml")
-    |> send_resp(200, tag_rss_feed(blog_posts, slug))
+    |> send_resp(200, build_rss(channel, blog_posts, base_url))
   end
 
-  defp rss_feed(blog_posts) do
-    base_url = TqmWeb.Endpoint.url()
-
+  defp build_rss(
+         %{title: title, description: description, self_link: self_link},
+         blog_posts,
+         base_url
+       ) do
     items =
       Enum.map_join(blog_posts, fn post ->
         """
@@ -44,41 +62,10 @@ defmodule TqmWeb.FeedController do
     <?xml version="1.0" encoding="UTF-8"?>
     <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
       <channel>
-        <title>Quigley Malcolm</title>
+        <title>#{title}</title>
         <link>#{base_url}</link>
-        <description>Blog posts by Quigley Malcolm</description>
-        <atom:link href="#{base_url}/blog/feed.xml" rel="self" type="application/rss+xml"/>
-        #{items}
-      </channel>
-    </rss>
-    """
-  end
-
-  defp tag_rss_feed(blog_posts, slug) do
-    base_url = TqmWeb.Endpoint.url()
-
-    items =
-      Enum.map_join(blog_posts, fn post ->
-        """
-            <item>
-              <title>#{xml_escape(post.title)}</title>
-              <link>#{base_url}/blog/#{post.id}</link>
-              <guid>#{base_url}/blog/#{post.id}</guid>
-              <pubDate>#{rss_date(post.published_at)}</pubDate>
-              <description><![CDATA[#{post.content}]]></description>
-              #{tag_categories(post)}
-            </item>
-        """
-      end)
-
-    """
-    <?xml version="1.0" encoding="UTF-8"?>
-    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-      <channel>
-        <title>Quigley Malcolm - #{xml_escape(slug)} posts</title>
-        <link>#{base_url}</link>
-        <description>Blog posts tagged &#34;#{xml_escape(slug)}&#34; by Quigley Malcolm</description>
-        <atom:link href="#{base_url}/blog/tags/#{slug}/feed.xml" rel="self" type="application/rss+xml"/>
+        <description>#{description}</description>
+        <atom:link href="#{self_link}" rel="self" type="application/rss+xml"/>
         #{items}
       </channel>
     </rss>
